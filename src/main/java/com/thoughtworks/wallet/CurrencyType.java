e18b306ab7043bb820dcca2bd6829b0e46be42f1
keerthana <keerthana.kasthuril@thoughtworks.com>
2021-01-23 15:55:35 +0530

    Add: Currency type enum

---
+++ src/main/java/com/thoughtworks/wallet/CurrencyType.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.wallet;
+
+public enum CurrencyType {
+    Rupee(74.85),
+    Dollar(1);
+
+    public double equivalence;
+
+    CurrencyType(double value) {
+        this.equivalence = value;
+    }
+};
